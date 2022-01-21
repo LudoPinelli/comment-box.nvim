061bc839129745345224345b1cb1780bc18ba668
LudoPinelli <93927273+LudoPinelli@users.noreply.github.com>
2022-01-21 05:35:05 +0100

    feat: expand box creation possibilities | add catalog

--- dev/init.lua
+++ dev/init.lua
@@ -1,7 +1,8 @@
 package.loaded["comment-box"] = nil
 package.loaded["dev"] = nil
+package.loaded["comment-box.catalog"] = nil
 
 vim.keymap.set("n", ",r", "<Cmd>luafile dev/init.lua<CR>", {})
-vim.keymap.set({ "n", "v" }, "<Leader>bb", "<Cmd>lua require('comment-box').lbox()<CR>", {})
-vim.keymap.set({ "n", "v" }, "<Leader>bc", "<Cmd>lua require('comment-box').cbox()<CR>", {})
-vim.keymap.set("n", "<Leader>bl", "<Cmd>lua require('comment-box').line()<CR>", {})
+vim.keymap.set({ "n", "v" }, "<Leader>bb", "<Cmd>lua require('comment-box').lbox(20)<CR>", {})
+vim.keymap.set({ "n", "v" }, "<Leader>bc", "<Cmd>lua require('comment-box').cbox(13)<CR>", {})
+vim.keymap.set("n", "<Leader>bl", "<Cmd>lua require('comment-box').line(9)<CR>", {})
