16f90cdc5d627e139eb9a72faaf4988a590aacb8
LudoPinelli <93927273+LudoPinelli@users.noreply.github.com>
2024-01-02 11:58:42 +0100

    remove the useless /dev folder

--- dev/init.lua
+++
@@ -1,9 +0,0 @@
-package.loaded["comment-box"] = nil
-package.loaded["dev"] = nil
-package.loaded["comment-box.catalog"] = nil
-package.loaded["comment-box.catalog_view"] = nil
-
-vim.keymap.set("n", ",r", "<Cmd>luafile dev/init.lua<CR>", {})
--- vim.keymap.set({ "n", "v" }, "<Leader>bb", "<Cmd>lua require('comment-box').lbox()<CR>", {})
--- vim.keymap.set({ "n", "v" }, "<Leader>bc", "<Cmd>lua require('comment-box').cbox()<CR>", {})
--- vim.keymap.set("n", "<Leader>bl", "<Cmd>lua require('comment-box').line()<CR>", {})
